3b92981ff662609b837b45c1ca718a191fe3b425
Mayaraj Mahabub (Raj) <official2050@yahoo.com>
2020-03-31 15:56:13 +0600

    Class-3 practice

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Web Course</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>New Course has been started!</h1>
+	<br>
+	<table>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+</body>
+</html>
